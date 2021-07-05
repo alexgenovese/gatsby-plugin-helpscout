cf2c78e0a83fe6fad82bb09857209bc303c8d4eb
Alex Genovese <423745+alexgenovese@users.noreply.github.com>
2021-07-05 12:49:13 +0200

    fix code

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,22 +1,26 @@
-'use strict';
+"use strict";
 
-var _react = require('react');
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
-var _react2 = _interopRequireDefault(_react);
+var _react = _interopRequireDefault(require("react"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-exports.onRenderBody = function (_ref, pluginOptions) {
-  var setPostBodyComponents = _ref.setPostBodyComponents;
-
+exports.onRenderBody = ({
+  setPreBodyComponents
+}, pluginOptions) => {
   if (process.env.NODE_ENV === 'production' || pluginOptions.includeInDevelopment) {
-    var code = pluginOptions.code,
-        sv = pluginOptions.sv;
-
-    return setPostBodyComponents([_react2.default.createElement('script', {
-      key: 'gatsby-plugin-helpscount',
+    const {
+      code
+    } = pluginOptions;
+    return setPreBodyComponents([/*#__PURE__*/_react.default.createElement("script", {
+      key: `gatsby-plugin-helpscout`,
       dangerouslySetInnerHTML: {
-        __html: '\n              !function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});\n              window.Beacon(\'init\', ' + code + ')\n          '
+        __html: `
+              !function(e,t,n){
+                t._settings={code:'${code}'};
+                function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)
+                window.Beacon('init', t._settings.code)
+              }(window,document,window.Beacon||function(){});
+          `
       }
     })]);
   }
